cd67d0f9f4dddd4940bfc88ee0688b4630be9120
Dasha <podolariya@gmail.com>
2025-03-18 14:35:26 -0400

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -2,10 +2,21 @@
 
 #include <iostream>
 using namespace std; 
-//first, we're doing the welcome message for our customers!
+
+void menu() {
+  cout << "~~~~~~~~~~~~~~~~~~ MENU ~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+  cout << "1. Pierogi with potato and cheese        $8.00" << endl;
+  cout << "2. Borscht                               $6.50" << endl;
+  cout << "3. Compot (beverage with berries)        $3.99" << endl;
+  cout << "4. The Monastic Hut                      $4.99" << endl;
+  cout << "5. Poppyseed Roll                        $7.50" << endl;
+}
+
+//first, we did the welcome message for our customers!
+
 int main() {
   cout << "***************************************************" << endl;
-  cout << "Welcome to our restaurant 'Puzata Maty'!" << endl;
+  cout << "Welcome to our restaurant 'Puzata Hata'!" << endl;
   cout << "***************************************************" << endl;
 
   //then, we created code to ask the customer for PIN to access the menu of our restaurant:
@@ -30,9 +41,12 @@ string entry;
 
  if ( entry == pin )
   cout << "\nPIN is approved. Here is our menu!" << endl;
+  menu();
+  
  else if ( tries <= max_tries )
   cout << "\nYou are out of attempts :( " << endl;
 
+// the we did another function for menu but we put it before main fuction for it to run properly
 }
 
-}
+
